fb7071b7ef4fba0bc88ee3d31372b6370ac5578b
w-foster <william.jfoster@yahoo.com>
2024-10-14 23:12:06 +0100

    Added endpoint comments

--- src/app.py
+++ src/app.py
@@ -5,30 +5,32 @@ import secrets
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32) # Hardcoded session key for DEVELOPMENT ONLY
 
-
+# Landing page; immediately starts new game
 @app.route("/")
 def landing():
     return new_game()
 
+# Initialises session score variables to zero
 @app.route("/new_game")
 def new_game():
     session["total_answers_count"] = 0
     session["correct_answers_count"] = 0
     return question()
 
+# Serves a randomly generated question
 @app.route("/question")
 def question():
     i = random.randint(0, 2)
     if i == 0:
         # addition
-        random_one = random.randint(3, 75)
-        random_two = random.randint(3, 75)
+        random_one = random.randint(7, 62)
+        random_two = random.randint(7, 62)
         random_operator = "+"
         session["correct_answer"] = random_one + random_two
     elif i == 1:
         # subtraction
-        random_one = random.randint(3, 54)
-        random_two = random.randint(3, 54)
+        random_one = random.randint(5, 48)
+        random_two = random.randint(5, 48)
         random_operator = "-"
         session["correct_answer"] = random_one - random_two
     elif i == 2:
@@ -37,20 +39,24 @@ def question():
         random_two = random.randint(3, 12)
         random_operator = "*"
         session["correct_answer"] = random_one * random_two
-    question_string = str(random_one) + " " + random_operator + " " + str(random_two)
+
     question_string = f"{random_one} {random_operator} {random_two}"
     return render_template("index.html", question=question_string)
 
+# Checks user's answer and serves a results page
 @app.route("/submit", methods=["POST"])
 def submit():
     input_answer = int(request.form.get("answer"))
     session["total_answers_count"] += 1
     if input_answer == session["correct_answer"]:
         session["correct_answers_count"] += 1
-        is_correct = True  # Used by front-end to show whether ans was correct or not
+        is_correct = True  # Used by front-end to display conditional messages
     else:
         is_correct = False
     return render_template("answer_result.html", correct_answer=session["correct_answer"], is_correct=is_correct, total_answers_count=session["total_answers_count"], correct_answers_count=session["correct_answers_count"])
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
