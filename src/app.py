47dc48c8f6862f7f1691dd98dfe40f9113b3fd1e
w-foster <william.jfoster@yahoo.com>
2024-10-14 22:39:37 +0100

    Added session variables to track score.

--- src/app.py
+++ src/app.py
@@ -1,41 +1,55 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import random
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(32) # Hardcoded session key for DEVELOPMENT ONLY
 
-
-correct_answer = 0 # this will store the correct answer to the current Q
 
 @app.route("/")
-def hello_world():
-    global correct_answer
-    random_one = random.randint(3, 75)
-    random_two = random.randint(3, 75)
+def landing():
+    return new_game()
+
+@app.route("/new_game")
+def new_game():
+    session["total_answers_count"] = 0
+    session["correct_answers_count"] = 0
+    return question()
+
+@app.route("/question")
+def question():
     i = random.randint(0, 2)
     if i == 0:
         # addition
+        random_one = random.randint(3, 75)
+        random_two = random.randint(3, 75)
         random_operator = "+"
-        correct_answer = random_one + random_two
+        session["correct_answer"] = random_one + random_two
     elif i == 1:
         # subtraction
+        random_one = random.randint(3, 54)
+        random_two = random.randint(3, 54)
         random_operator = "-"
-        correct_answer = random_one - random_two
+        session["correct_answer"] = random_one - random_two
     elif i == 2:
         # multiplication
+        random_one = random.randint(3, 12)
+        random_two = random.randint(3, 12)
         random_operator = "*"
-        correct_answer = random_one * random_two
+        session["correct_answer"] = random_one * random_two
     question_string = str(random_one) + " " + random_operator + " " + str(random_two)
+    question_string = f"{random_one} {random_operator} {random_two}"
     return render_template("index.html", question=question_string)
-
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    global correct_answer
     input_answer = int(request.form.get("answer"))
-    if input_answer == correct_answer:
-        is_correct = True
+    session["total_answers_count"] += 1
+    if input_answer == session["correct_answer"]:
+        session["correct_answers_count"] += 1
+        is_correct = True  # Used by front-end to show whether ans was correct or not
     else:
         is_correct = False
-    return render_template("hello.html", correct_answer=correct_answer, is_correct=is_correct)
+    return render_template("hello.html", correct_answer=session["correct_answer"], is_correct=is_correct, total_answers_count=session["total_answers_count"], correct_answers_count=session["correct_answers_count"])
 
 
