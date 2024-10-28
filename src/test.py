ae34ab1c254ed68667f602bdf2635f55de446dfd
akcchi <161343155+akcchi@users.noreply.github.com>
2024-10-28 14:50:09 +0000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 from app import process_query
 
 def test_knows_larger_numbers():
-    assert "Which of the following numbers is the largest: 47, 32, 58?" == str(58)
-
+    assert ("Which of the following numbers is the largest: 47, 32, 58?" 
+            == str(58))
