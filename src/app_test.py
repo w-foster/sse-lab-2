f2849a6141381378f59743237eb84b0afcceba11
w-foster <william.jfoster@yahoo.com>
2024-10-28 15:12:54 +0000

    Resetting

--- src/app_test.py
+++ src/app_test.py
@@ -1,10 +1,6 @@
 from app import process_query
 
 
-def test_knows__about_your_name():
-    assert process_query("What is your name?") == "turtle"
-
-
 def test_knows_about_dinosaurs():
     dino = "Dinosaurs ruled the Earth 200 million years ago"
     assert process_query("dinosaurs") == dino
@@ -12,7 +8,3 @@ def test_knows_about_dinosaurs():
 
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
-
-
-def test_sum():
-    assert process_query("What is 94 + 3?") == str(94 + 3)
--- src/test.py
+++
@@ -1,7 +0,0 @@
-from app import process_query
-
-
-def test_knows_larger_numbers():
-    assert (process_query("Which of the following numbers" +
-                          "is the largest: 47, 32, 58?")
-            == str(58))
